df8fe2c854681b780935de05917c27a266bb502a
igsanceda <igsanceda@up.edu.ph>
2023-08-05 22:46:45 +0800

    Change default title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
     # Returns the full title on a per-page basis.
     def full_title(page_title = '')
-        base_title = "Cinema Booking"
+        base_title = "Cinema Booking App"
         if page_title.empty?
             base_title
         else
--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   
   def setup
-    @base_title = "Cinema Booking"
+    @base_title = "Cinema Booking App"
   end
 
   test "should get home" do
