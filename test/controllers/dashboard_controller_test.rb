d554d665b6c651ded8ce9a831289da6018e2c8ca
igsanceda <igsanceda@up.edu.ph>
2023-08-05 22:39:19 +0800

    Added title to home page

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @base_title = "Cinema Booking"
+  end
+
   test "should get home" do
-    get dashboard_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 end
