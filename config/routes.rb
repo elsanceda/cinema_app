dd6d47f9f97a337c7c758a2850d5e8999f0fe860
igsanceda <igsanceda@up.edu.ph>
2023-08-06 13:09:06 +0800

    Add Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "dashboard#home"
-  get  "/signup",  to: "users#new"
+  root 'dashboard#home'
+  get  '/signup',  to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users, only: [:new, :create]
 end
