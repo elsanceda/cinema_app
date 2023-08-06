778f9af18cba8e1b857f913aaaefb9056de696e8
igsanceda <igsanceda@up.edu.ph>
2023-08-06 14:15:56 +0800

    Implement log-out

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -24,3 +24,50 @@ class InvalidPasswordTest < UsersLogin
     assert flash.empty?
   end
 end
+
+class ValidLogin < UsersLogin
+
+  def setup
+    super
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'Password1' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+
+  test "valid login" do
+    assert is_logged_in?
+    assert_redirected_to root_path
+  end
+
+  test "redirect after login" do
+    follow_redirect!
+    assert_template 'dashboard/home'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+  end
+end
+
+class Logout < ValidLogin
+
+  def setup
+    super
+    delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+
+  test "successful logout" do
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
+
+  test "redirect after logout" do
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+  end
+end
