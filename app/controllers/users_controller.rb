95ad38d3214b9f7e856fd46a9f9b178155115879
igsanceda <igsanceda@up.edu.ph>
2023-08-06 12:46:53 +0800

    Add signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # TODO
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:full_name, :email, :mobile_number,
+                                   :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "dashboard#home"
   get  "/signup",  to: "users#new"
+  resources :users, only: [:new, :create]
 end
