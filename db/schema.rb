874f9e2334b3d494d19ec0c3287cb645549053d5
igsanceda <igsanceda@up.edu.ph>
2023-08-07 12:56:41 +0800

    Add timeslot model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_06_125650) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_07_044458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_06_125650) do
     t.text "synopsis"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "timeslots", force: :cascade do |t|
+    t.time "time", default: "2000-01-01 02:00:00"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["time"], name: "index_timeslots_on_time", unique: true
   end
 
   create_table "users", force: :cascade do |t|
